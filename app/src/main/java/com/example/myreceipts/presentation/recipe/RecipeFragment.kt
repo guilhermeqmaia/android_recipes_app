3fa3ae853016989ba19ed21e91a0bdb51f9a7457
guilhermeqmaia <guilherme.q.maia.contato@gmail.com>
2023-10-10 23:01:46 -0300

    feat: create adapter

--- app/src/main/java/com/example/myreceipts/presentation/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/myreceipts/presentation/recipe/RecipeFragment.kt
@@ -6,18 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.myrecipes.databinding.FragmentRecipeBinding
+import com.example.myreceipts.presentation.recipe.adapter.RecipeAdapter
 
 class RecipeFragment : Fragment() {
 
     private lateinit var binding: FragmentRecipeBinding
+    private val adapter by lazy { RecipeAdapter() }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentRecipeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupListeners()
+        setupAdapter()
+    }
+
+    private fun setupListeners() {
+        binding.fabRecipe.setOnClickListener {
+            //@TODO SHOW DIALOG
+        }
+    }
+
+    private fun setupAdapter() {
+        binding.rvRecipes.adapter = adapter
     }
 
 }
